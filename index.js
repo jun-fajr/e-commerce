642afd54a5e981ec23f600a5959b3cb81d72799d
Jun al-Fajr <jun.fajr@gmail.com>
2023-01-04 17:28:23 +0700

    create api register user

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-const express = require("express");
-const app = express();
-const dotenv = require("dotenv").config();
-const PORT = process.env.PORT || 4000;
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 4000
+const dbConnect = require('./config/dbConnect')
+const authRouter = require('./routes/authRoute')
+const bodyParser = require('body-parser')
+
+dbConnect()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+// app.use('/', (req, res) => {
+//   res.send('Hello from server side')
+// })
+app.use('/api/user', authRouter)
 
 app.listen(PORT, () => {
-  console.log("Server listening on port " + PORT);
-});
+  console.log(`Server listening on port  ${PORT}`)
+})
